094acd80744279323f2fc806bef8f3db039e06d8
Marliton <ninjaepicoxd@gmail.com>
2026-01-08 23:49:48 -0500

    .

--- PaginaPrincipal/codigoEmpleado.js
+++
@@ -1,6 +0,0 @@
-const btnMenu = document.getElementById('btnMenu');
-const sidebar = document.getElementById('sidebar');
-
-btnMenu.addEventListener('click', () => {
-    sidebar.classList.toggle('active');
-});
